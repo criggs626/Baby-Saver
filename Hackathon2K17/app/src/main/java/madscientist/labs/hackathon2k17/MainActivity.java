eb9062b17e5802e6cbe73a01657ec60e9faf27f1
Caleb's PC <criggs2892@flpoly.org>
2017-03-26 06:28:02 -0400

    Added initial files including functioning android application to view the status and arduino code to read inputs and send data

---
+++ Hackathon2K17/app/src/main/java/madscientist/labs/hackathon2k17/MainActivity.java
@@ -0,0 +1,270 @@
+package madscientist.labs.hackathon2k17;
+
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import android.bluetooth.BluetoothDevice;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.security.spec.ECField;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.UUID;
+
+
+public class MainActivity extends AppCompatActivity {
+    //these values are specific to the Grove BLE V1 - update if you're using a different module
+    private static String GROVE_SERVICE = "19B10001-E8F2-537E-4F6C-D104768A1214";
+    private static String CHARACTERISTIC_TX = "19B10001-E8F2-537E-4F6C-D104768A1214";
+
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final long SCAN_PERIOD = 5000; //5 seconds
+    private static final String DEVICE_NAME = "BBYSAV"; //display name for Grove BLE
+
+    private BluetoothAdapter mBluetoothAdapter;//our local adapter
+    private BluetoothGatt mBluetoothGatt; //provides the GATT functionality for communication
+    private BluetoothGattService mBluetoothGattService; //service on mBlueoothGatt
+    private static List<BluetoothDevice> mDevices = new ArrayList<BluetoothDevice>();//discovered devices in range
+    private BluetoothDevice mDevice; //external BLE device (Grove BLE module)
+
+    private TextView mainText;
+    private Timer mTimer;
+
+    boolean present;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mainText=(TextView)findViewById(R.id.mainText);
+        mTimer = new Timer();
+
+        //check to see if Bluetooth Low Energy is supported on this device
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, "BLE not supported on this device", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        statusUpdate("BLE supported on this device");
+
+        //get a reference to the Bluetooth Manager
+        final BluetoothManager mBluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = mBluetoothManager.getAdapter();
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, "BLE not supported on this device", Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+
+        //Open settings if Bluetooth isn't enabled
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth disabled", Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+
+        //try to find the Grove BLE V1 module
+
+
+    }
+
+    public void myTest(View v){
+        searchForDevices();
+    }
+
+
+    private void searchForDevices ()
+    {
+        statusUpdate("Searching for devices ...");
+
+        if(mTimer != null) {
+            mTimer.cancel();
+        }
+
+        scanLeDevice();
+        mTimer = new Timer();
+        mTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                statusUpdate("Search complete");
+                findGroveBLE();
+            }
+        }, SCAN_PERIOD);
+    }
+
+    private void findGroveBLE ()
+    {
+        if(mDevices == null || mDevices.size() == 0)
+        {
+            statusUpdate("No BLE devices found");
+            return;
+        }
+        else if(mDevice == null)
+        {
+            statusUpdate("Unable to find Grove BLE");
+            return;
+        }
+        else
+        {
+            statusUpdate("Found Device");
+            statusUpdate("Address: " + mDevice.getAddress());
+            connectDevice();
+        }
+    }
+
+    private boolean connectDevice ()
+    {
+        BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(mDevice.getAddress());
+        if (device == null) {
+            statusUpdate("Unable to connect");
+            return false;
+        }
+        // directly connect to the device
+        statusUpdate("Connecting ...");
+        mBluetoothGatt = device.connectGatt(this, false, mGattCallback);
+        return true;
+    }
+
+    private final BluetoothGattCallback mGattCallback = new BluetoothGattCallback() {
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                statusUpdate("Connected");
+                statusUpdate("Searching for services");
+                mBluetoothGatt.discoverServices();
+            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                statusUpdate("Device disconnected");
+            }
+        }
+
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                List<BluetoothGattService> gattServices = mBluetoothGatt.getServices();
+
+                for(BluetoothGattService gattService : gattServices) {
+                    String temp = gattService.getUuid().toString();
+                    statusUpdate("Service discovered: " + gattService.getUuid());
+                    if(GROVE_SERVICE.equals(temp));
+                    {
+                        mBluetoothGattService = gattService;
+                        statusUpdate("Found communication Service");
+                            sendMessage();
+                    }
+                }
+            } else {
+                statusUpdate("onServicesDiscovered received: " + status);
+            }
+        }
+
+        @Override
+        public void onCharacteristicRead(BluetoothGatt gatt,BluetoothGattCharacteristic characteristic,int status){
+            byte[] value = characteristic.getValue();
+            StringBuilder sb = new StringBuilder();
+            for (byte b : value) {
+                sb.append(String.format("%02X", b));
+            }
+            String temp=sb.toString();
+            present=temp.equals("01");
+            Log.e("Package","Read characteristic value: " + temp);
+            statusUpdate("It worked");
+        }
+    };
+
+    private void sendMessage ()
+    {
+        if (mBluetoothGattService == null)
+            return;
+
+        statusUpdate("Finding Characteristic...");
+        BluetoothGattCharacteristic gattCharacteristic =
+                mBluetoothGattService.getCharacteristic(UUID.fromString(CHARACTERISTIC_TX));
+
+        if(gattCharacteristic == null) {
+            statusUpdate("Couldn't find TX characteristic: " + CHARACTERISTIC_TX);
+            return;
+        }
+
+        statusUpdate("Found TX characteristic: " + CHARACTERISTIC_TX);
+
+        statusUpdate("Reading message...");
+
+        String msg = "Hello Grove BLE";
+        mBluetoothGatt.readCharacteristic(gattCharacteristic);
+    }
+
+    private void scanLeDevice() {
+        new Thread() {
+
+            @Override
+            public void run() {
+                mBluetoothAdapter.startLeScan(mLeScanCallback);
+
+                try {
+                    Thread.sleep(SCAN_PERIOD);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                mBluetoothAdapter.stopLeScan(mLeScanCallback);
+            }
+        }.start();
+    }
+
+    private BluetoothAdapter.LeScanCallback mLeScanCallback = new BluetoothAdapter.LeScanCallback() {
+
+        @Override
+        public void onLeScan(final BluetoothDevice device, final int rssi,
+                             byte[] scanRecord) {
+            if (device != null) {
+                if (mDevices.indexOf(device) == -1)//to avoid duplicate entries
+                {
+                    if (DEVICE_NAME.equals(device.getName())) {
+                        mDevice = device;//we found our device!
+                    }
+                    mDevices.add(device);
+                    statusUpdate("Found device " + device.getName());
+                }
+            }
+        }
+    };
+
+    //output helper method
+    private void statusUpdate (final String msg) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Log.w("BLE", msg);
+                if(present){
+                    mainText.setText("Package Present");
+                }
+                else{
+                    mainText.setText("Package Absent");
+                }
+            }
+        });
+    }
+}
---
+++ arduino/BBYSAV.ino
@@ -0,0 +1,96 @@
+/*This code was written for the 2017 PolyHacks event.
+  The code below is the intellectual property of Samuel Roberti, Caleb Riggs, and Brian Walsh.
+  Use for all baby-saving purposes is authorized.
+ */
+
+
+
+#include <CurieBLE.h>
+
+
+BLEPeripheral blePeripheral;  // BLE Peripheral Device 
+BLEService BSService("19B10000-E8F2-537E-4F6C-D104768A1214"); // BLE Baby-Saving Service
+
+
+
+BLEUnsignedCharCharacteristic occCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify);// This characteristic is read by the Android app.  It is always either a 0 or 1.
+
+#define soundPin A0// The sound sensor
+#define pressPin 4// The button sensor
+#define tempPin A2// The temperature sensor
+#define beepPin 3// The beeping alarm
+
+
+
+void setup() {
+  pinMode(soundPin, INPUT);// Initialize pin modes.
+  pinMode(pressPin, INPUT);
+  pinMode(tempPin, INPUT);
+  pinMode(beepPin, OUTPUT);
+
+  blePeripheral.setLocalName("BBYSAV");                     //Sets the name of the device, to be broadcast over BLE.
+  blePeripheral.setAdvertisedServiceUuid(BSService.uuid()); // Assigns the main service to be advertised over BLE.
+
+  blePeripheral.addAttribute(BSService);                    //Adds the main service and characteristic as attributes of the peripheral. 
+  blePeripheral.addAttribute(occCharacteristic);
+  
+  blePeripheral.begin();                                    // Begin broadcasting.
+                           
+  
+}
+
+void loop() {
+  
+  
+  
+  if(analogRead(soundPin)>900){    // If a noise above a certain volume is detected(which, in most situations, would be the car door closing), check pressure sensors to see if the vehicle still has baby inside.
+   if(digitalRead(pressPin)==HIGH){
+     occCharacteristic.setValue(1);
+  }
+   else{
+     occCharacteristic.setValue(0);
+  }
+ }
+   int a = analogRead(tempPin);  // Calculate current temperature.
+   int B = 4275;
+
+   float R = 1023.0/((float)a)-1.0;
+   R = 100000.0*R;
+
+   float temperature=1.0/(log(R/100000.0)/B+1/298.15)-273.15;
+  if(temperature > 90){                                         // If the temperature is above the threshhold of danger, check pressure sensor.  If the baby is present, sound a loud beeper.
+    if(digitalRead(pressPin)==HIGH){
+      digitalWrite(beepPin, HIGH);
+      delay(200);
+      digitalWrite(beepPin, LOW);
+      delay(200);
+      digitalWrite(beepPin, HIGH);
+      delay(200);
+      digitalWrite(beepPin, LOW);
+      delay(200);
+      digitalWrite(beepPin, HIGH);
+      delay(200);
+      digitalWrite(beepPin, LOW);
+      delay(200);
+      digitalWrite(beepPin, HIGH);
+      delay(200);
+      digitalWrite(beepPin, LOW);
+      delay(200);
+      digitalWrite(beepPin, HIGH);
+      delay(500);
+      digitalWrite(beepPin, LOW);
+      delay(200);
+      
+    }
+    
+  }
+
+  
+  }
+
+  
+
+  
+
+
+
